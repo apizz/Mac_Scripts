1b388edd70a1466cdf586a44be7fda2ea4c11d85
AP Orlebeke <aporlebeke@gmail.com>
2016-07-16 12:56:46 -0400

    Initial commit of OSX_Computer_Auth

---
+++ OSX_Computer_Auth/OSX_Computer_Auth.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+#####
+# OSX Machine auth for 802.1x profile with Active Directory
+# get AD machine user/pass and put into 802.1x profile template
+# install the profile
+#
+# sed has it's own uses for '&' and '\' in replacements
+# and the randomly generated password sometimes has them
+# So, trap and escape them before feeding into sed
+#
+# put the host name in 'host/computername.domain.com' format
+##
+# Originally by Dan Pereira ~2012
+# added traps and host name modification
+# thp 7/16/14
+#####
+DOMAIN="themastersschool.com"
+PASS=`sudo /usr/bin/security find-generic-password -s "/Active Directory/MASTERS" -w /Library/Keychains/System.keychain`
+USER=`/usr/sbin/dsconfigad -show | awk '/Computer *Account/ { print $4 }'`
+
+# trap '\' and escape them
+if [[ ${PASS} =~ '\' ]]; then
+PASS=$(echo "$PASS" | sed 's/\\/\\\\/g') 
+fi
+
+# trap '&' and escape them
+if [[ ${PASS} =~ '&' ]]; then
+PASS=$(echo "$PASS" | sed 's/&/\\&/g') 
+fi
+
+# format username as hostname
+USER=`echo $USER | tr -d '$'`
+USER="host\/${USER}.${DOMAIN}"
+
+# change template file
+PROPATH='/Library/Scripts/Profiles'
+PROFILE='faculty_machine_profile.mobileconfig'
+
+sed -i .bak 's/TESTPASS/'${PASS}'/' ${PROPATH}/${PROFILE}
+sed -i .bak 's/TESTUSER/'${USER}'/' ${PROPATH}/${PROFILE}
+/usr/bin/profiles -I -F ${PROPATH}/${PROFILE}
+rm -f ${PROPATH}/${PROFILE}.bak
+
+
+exit

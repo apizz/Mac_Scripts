2ad7b2702818dade4d672dde70ec067a68a6a148
AP Orlebeke <aporlebeke@gmail.com>
2016-07-16 20:49:32 -0400

    Updated script

--- Download_Install_TeamViewer/Download_Install_TeamViewer.sh
+++ Download_Install_TeamViewer/Download_Install_TeamViewer.sh
@@ -1,3 +1,86 @@
 #!/bin/bash
 
+# Downloads TeamViewer 11 Host package, effectively going to get.teamviewer.com/yourcustomURL in a browser.
+# 
+# To get the full download link, enter your custom module link in a browser and right-click on link in the middle of
+# the window.  You will need this for the curl command to download the install PKG.
+#
+
+########################################################
+logfile="/path/to/TeamViewerInstall.log"
+downloaddir="/Users/Shared"
+PKG="InstallTeamViewer-XXXXXXXXXX.pkg"
+
+# Create a function to echo output and write to a log file
+writelog() {
+	echo "${1}"	"${2}" "${3}" "${4}"
+	echo $(date) "${1}"	"${2}" "${3}" "${4}" >> $logfile
+}
+########################################################
+
+# Check for TeamViewer.log
+if [ -f "/Library/Logs/TMSTech/TeamViewerInstall.log" ]; then
+    writelog "CHECK: TeamViewerInstall.log Present."
+else
+    /usr/bin/touch $logfile
+    writelog "CREATED: TeamViewerInstall.log"
+    /bin/chmod 777 $logfile
+    if [ $? = 0 ]; then
+        writelog "SUCCESSFUL: Set TeamViewerInstall.log Permissions."
+    else
+        writelog "FAILED: Set TeamViewerInstall.log Permissions."
+    fi
+fi
+
+writelog "------- START -------"
+
+# Set download directory to working directory.
+cd "$downloaddir"
+
+if [ "$(pwd)" = "$downloaddir" ]; then
+    writelog "Working Directory Set to Download Directory: Successful."
+else
+    writelog "Working Directory Set to Download Directory: Failed."
+fi
+
+writelog "DOWNLOADING: TeamViewer Install PKG"
+
+# Enter your full TeamViewer download link in the single quotes.
+# If you are on version 10 of TeamViewer, it will be ../version_10x/.. rather than version_11x
+#
+# ex. https://download.teamviewer.com/download/version_11x/CustomDesign/Install%20TeamViewerHost-XXXXXXXXXX.pkg
+#
+# In order to have your custom TeamViewer branding apply, the PKG variable MUST have 10-digit 
+# branding ID in the PKG name.
+
+/usr/bin/curl 'https://download.teamviewer.com/download/version_1Xx/CustomDesign/Install....-XXXXXXXXXX.pkg' -o $PKG
+
+if [ -f "$downloaddir/$PKG" ]; then
+    # Installs package
+    writelog "INSTALLING: TeamViewer module ..."
+    /usr/sbin/installer -pkg "$downloaddir/$PKG" -target /
+    if [ $? = 0 ]; then
+        writelog "TeamViewer Install: Successful."
+        
+        writelog "DELETING: TeamViewer Host PKG ..."
+        sudo /bin/rm -rf "$downloaddir/$PKG"
+
+        if [ ! -f "$downloaddir/$PKG" ]; then
+            writelog "TeamViewer Install PKG Deletion: Successful."
+        else
+            writelog "TeamViewer Install PKG Deletion: Failed."
+        fi
+        
+        writelog "Launching TeamViewer for the first time ..."
+        # If using TeamViewer Host, put TeamViewerHost in double quotes.
+        open -a "TeamViewer"
+
+        writelog "Script Complete: TeamViewer Installed!"
+    else
+        writelog "TeamViewer Install: Failed."
+    fi
+fi
+
+writelog "------- DONE -------"
+
 exit

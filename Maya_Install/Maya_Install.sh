a88933c4528d35eea57ed93bf0680decf710c83f
AP Orlebeke <aporlebeke@gmail.com>
2016-07-18 10:10:42 -0400

    Minor syntax changes

--- Maya_Install/Maya_Install.sh
+++ Maya_Install/Maya_Install.sh
@@ -5,28 +5,28 @@
 # Make sure you change XXX-XXXXXXXX serial & product key.
 
 INSTALLDIR="/Users/Shared"
-MAYA="/Install\ Maya\ 2016.app"
-MAYAINSTALLER="Install\ Maya\ 2016.app/Contents/MacOS/setup"
+MAYA="Install Maya 2016.app"
+MAYAINSTALLER="Install Maya 2016.app/Contents/MacOS/setup"
 
 /bin/echo "Beginning Maya 2016 install."
 
 #Install Maya 2016
-$INSTALLDIR/$MAYA --noui --force --serial_number=XXX-XXXXXXXX --product_key=XXXXX --license_type=<kStandalone or kNetwork> #--server_name=<name>
+"$INSTALLDIR"/"$MAYAINSTALLER" --noui --force --serial_number=XXX-XXXXXXXX --product_key=XXXXX --license_type=<kStandalone or kNetwork> #--server_name=<name>
 
 if [ $? = 0 ]; then
     /bin/echo "Maya 2016 Install: Successful."
     /bin/echo "Attempting to delete Maya installer."
     
-    /bin/rm -rf $INSTALLDIR/$MAYA
+    /bin/rm -rf "$INSTALLDIR"/"$MAYA"
     
-    if [ ! -d $INSTALLDIR/$MAYA ]; then
+    if [ ! -d "$INSTALLDIR"/"$MAYA" ]; then
         /bin/echo "Maya 2016 Installer Deletion: Successful."
     else
         /bin/echo "Maya 2016 Installer Deletion: Failed."
     fi
         
 else
-    /bi/echo "Maya 2016 Install: Failed."
+    /bin/echo "Maya 2016 Install: Failed."
 fi
 
 /bin/echo "DONE"
